a1d462758e37a392f1b140bca60c7a334050899d
ПΣMӨ <oshadairesh10@gmail.com>
2026-07-04 13:34:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,126 @@
+# Xray + Argo for Express.js PaaS
+
+Argo connects various services to connect millions of users on platforms without public networks. --- Born for the JS platform
+
+* * *
+
+# Table of Contents
+
+- [Project Features](README.md#project-features)
+- [Deployment](README.md#deployment)
+- [Deployment Highlights in Glitch](README.md#deployment-highlights-in-glitch)
+- [Deployment Highlights in Daki](README.md#deployment-highlights-in-daki)
+- [Deployment of ttyd webssh / filebrowser webftp](README.md#deployment-of-ttyd-webssh--filebrowser-webftp)
+- [Thanks to the following authors for their articles and projects](README.md#thanks-to-the-following-authors-for-their-articles-and-projects)
+- [Disclaimer](README.md#disclaimer)
+
+* * *
+
+## Project Features:
+* This project is used to deploy Xray on the Express.js PaaS platform, using the Argo + Xray + WebSocket + TLS solution
+* Unlock ChatGPT
+* View system information in the browser, convenient and intuitive
+* Use CloudFlare Argo tunnel, directly select + tunnel, CDN no longer needs to be workers
+* Backflow and diversion, support Xray's 4 mainstream protocols: vless / vmess / trojan / shadowsocks
+* vmess and vless uuid, trojan and shadowsocks password, ws path of each protocol can be customized or use the default value
+* Integrate Nezha probe, you can freely choose whether to install
+* Front-end js timed keep alive, users who know how to play can modify the interval time according to specific circumstances
+* Node information is output in V2rayN / Clash / Little Rocket link mode
+* Xray file recompiles official file to increase confidentiality, and modifies the display information at runtime. The file is: https://github.com/XTLS/Xray-core/blob/main/core/core.go
+* You can use webssh and webftp with a browser, which is more convenient for managing the system
+
+<img width="718" alt="image" src="https://user-images.githubusercontent.com/92626977/215277537-ff358dc1-7696-481f-b8e4-74f0cdff30f4.png">
+
+## Deployment:
+
+### Variables used by the PaaS platform:
+
+* Modify the username and password for querying the web page in lines 1 and 2 of the `server.js` file
+| Variable name | Required | Default value | Remarks |
+| ------------ | ------ | ------ | ------ |
+| WEB_USERNAME | Yes | admin | Username of the web page |
+| WEB_PASSWORD | Yes | password | Password of the web page |
+
+<img width="939" alt="image" src="https://user-images.githubusercontent.com/92626977/221387298-4183a1d6-ae14-45f9-b498-1789a4f7117e.png">
+
+* Modify the first 4-15 lines of the `entrypoint.sh` file; the authentication of accessing the page is modified in the 1st and 2nd lines of the `server.js` file. Required
+| Variable name | Required | Default value | Remarks |
+| ------------ | ------ | ------ | ------ |
+| UUID | No | de04add9-5c68-8bab-950c-08cd5320df18 | Can be generated online https://www.zxgj.cn/g/uuid |
+| WSPATH | No | argo | Do not start with /, the protocol path is `/WSPATH-protocol`, such as `/argo-vless`,`/argo-vmess`,`/argo-trojan`,`/argo-shadowsocks` |
+| NEZHA_SERVER | No | | IP or domain name of the Nezha probe server |
+| NEZHA_PORT | No | | Port of the Nezha probe server |
+| NEZHA_KEY | No | | Nezha probe client-specific key |
+| NEZHA_TLS | No | | Whether to enable SSL/TLS encryption for the Nezha probe. If not, please delete it. If you want to enable it, fill in "1" |
+| ARGO_AUTH | No | | Argo's token or json value. The json value can be easily obtained from the following website without binding a card: https://fscarmen.cloudflare.now.cc/ |
+| ARGO_DOMAIN | No | | Argo's domain name. It must be filled in together with ARGO_DOMAIN to take effect |
+| SSH_DOMAIN | No | | The domain name, username and password of webssh are <WEB_USERNAME> and <WEB_PASSWORD> |
+| FTP_DOMAIN | No | | The domain name, username and password of webftp are <WEB_USERNAME> and <WEB_PASSWORD> |
+
+<img width="1301" alt="image" src="https://user-images.githubusercontent.com/92626977/226095672-ecbfc8e7-80f3-4821-abb4-df75c4ece483.png">
+
+* js to be applied
+| Command | Description |
+| ---- |------ |
+| <URL>/list | View node data |
+| <URL>/status | View background process |
+| <URL>/listen | View background listening port |
+| <URL>/test | Test whether it is a read-only system |
+
+## Deployment highlights in Glitch
+
+Here we only show the highlights, for more details, please refer to the project: https://github.com/fscarmen2/X-for-Glitch
+
+<img width="1105" alt="image" src="https://user-images.githubusercontent.com/92626977/214567653-768f4f91-13b5-4205-9118-f5510081e260.png">
+
+<img width="1680" alt="image" src="https://user-images.githubusercontent.com/92626977/215279773-d550494e-647b-42e8-b5dc-5d32679fbf9e.jpg">
+
+<img width="322" alt="image" src="https://user-images.githubusercontent.com/92626977/214568380-b07dd83b-a4d6-43fe-9ead-79f1393e909c.png">
+
+## Deployment highlights in Daki
+
+<img width="1198" alt="image" src="https://user-images.githubusercontent.com/92626977/212642015-e84e07de-9f07-466d-b446-8cd8431e7220.png">
+
+<img width="1198" alt="image" src="https://user-images.githubusercontent.com/92626977/212642096-dfcce6d1-d6b2-4b55-9b94-995e5561ac44.png">
+
+<img width="1198" alt="image" src="https://user-images.githubusercontent.com/92626977/212642206-6b12179d-b35a-4a1e-b4e1-963b537c7693.png">
+
+<img width="1547" alt="image" src="https://user-images.githubusercontent.com/92626977/214568691-54cc283b-614f-4fe7-8782-c48ed46cff31.png">
+
+<img width="1664" alt="image" src="https://user-images.githubusercontent.com/92626977/214580345-765231a7-ec63-4564-a188-ceae28308258.png">
+
+<img width="1137" alt="image" src="https://user-images.githubusercontent.com/92626977/215279783-0400e80e-83be-4142-8592-2385c54e36e6.jpg">
+
+<img width="322" alt="image" src="https://user-images.githubusercontent.com/92626977/214580604-8d4f6454-3b78-41a9-b765-cff714b85638.png">
+
+## ttyd webssh / filebrowser webftp deployment
+
+* Principle
+```
++---------+ argo +---------+ http +--------+ ssh +-----------+
+| browser | <==========> | CF edge | <==========> | ttyd | <=======> | ssh server|
++---------+ argo +---------+ websocket +--------+ ssh +-----------+
+
++---------+ argo +---------+ http +--------------+ ftp +-----------+
+| browser | <==========> | CF edge | <=========> | filebrowser | <=======> | ftp server|
++---------+ argo +---------+ websocket +--------------+ ftp +-----------+
+
+```
+
+* Tunnel built using Json method
+
+<img width="1643" alt="image" src="https://user-images.githubusercontent.com/92626977/235453084-a8c55417-18b4-4a47-9eef-ee3053564bff.png">
+
+<img width="1303" alt="image" src="https://github.com/fscarmen2/aa/assets/92626977/652ef3ff-c9a9-4771-92c8-bab6c516abeb">
+
+<img width="1001" alt="image" src="https://github.com/fscarmen2/aa/assets/92626977/5b5e0143-ba5a-4b6a-a7fd-e77ef9d0703e">
+
+<img width="1527" alt="image" src="https://github.com/fscarmen2/rrr/assets/92626977/91cece0d-cc61-4681-8eae-03f961a16976">
+
+## Thanks to the following authors for their articles and projects:
+* The front-end JS is based on the project of Nike Jeff, and is modified for universality and extended functions, https://github.com/hrzyang/glitch-trojan
+* The back-end is all original, and the source must be indicated if reproduced.
+
+## Disclaimer:
+* This program is for learning and understanding only, not for profit, please delete it within 24 hours after downloading, It cannot be used for any commercial purpose. The text, data and pictures are all copyrighted. If reproduced, the source must be indicated.
+* The use of this program must comply with the deployment disclaimer. The use of this program must comply with the laws and regulations of the deployment server location, the country where it is located and the country where the user is located. The program author is not responsible for any improper behavior of the user.
---
+++ index.js
@@ -0,0 +1,264 @@
+const username = process.env.WEB_USERNAME || "admin";
+const password = process.env.WEB_PASSWORD || "password";
+const url = "http://127.0.0.1";
+const port = process.env.PORT || 3000; /* Modify here when irregular port is allocated by container platform */
+const express = require("express");
+const app = express();
+var exec = require("child_process").exec;
+const os = require("os");
+const { legacyCreateProxyMiddleware } = require("http-proxy-middleware");
+var request = require("request");
+var fs = require("fs");
+var path = require("path");
+const auth = require("basic-auth");
+
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+// Page access password
+app.use((req, res, next) => {
+  const user = auth(req);
+  if (user && user.name === username && user.pass === password) {
+    return next();
+  }
+  res.set("WWW-Authenticate", 'Basic realm="Node"');
+  return res.status(401).send();
+});
+
+// Get system process table
+app.get("/status", function (req, res) {
+  let cmdStr = "ps -ef";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.type("html").send("<pre>Command execution error:\n" + err + "</pre>");
+    }
+    else {
+      res.type("html").send("<pre>Get system process table:\n" + stdout + "</pre>");
+    }
+  });
+});
+
+// Get system listening ports
+app.get("/listen", function (req, res) {
+    let cmdStr = "ss -nltp";
+    exec(cmdStr, function (err, stdout, stderr) {
+      if (err) {
+        res.type("html").send("<pre>Command execution error:\n" + err + "</pre>");
+      }
+      else {
+        res.type("html").send("<pre>Get system listening ports:\n" + stdout + "</pre>");
+      }
+    });
+  });
+
+
+// Get node data
+app.get("/list", function (req, res) {
+    let cmdStr = "cat list";
+    exec(cmdStr, function (err, stdout, stderr) {
+      if (err) {
+        res.type("html").send("<pre>Command execution error:\n" + err + "</pre>");
+      }
+      else {
+        res.type("html").send("<pre>Node data:\n\n" + stdout + "</pre>");
+      }
+    });
+  });
+
+// Get system version and memory information
+app.get("/info", function (req, res) {
+  let cmdStr = "cat /etc/*release | grep -E ^NAME";
+  exec(cmdStr, function (err, stdout, stderr) {
+    if (err) {
+      res.send("Command execution error: " + err);
+    }
+    else {
+      res.send(
+        "Command execution result:\n" +
+          "Linux System:" +
+          stdout +
+          "\nRAM:" +
+          os.totalmem() / 1000 / 1000 +
+          "MB"
+      );
+    }
+  });
+});
+
+// File system read-only test
+app.get("/test", function (req, res) {
+  let cmdStr = 'mount | grep " / " | grep "(ro," >/dev/null';
+  exec(cmdStr, function (error, stdout, stderr) {
+    if (error !== null) {
+      res.send("System permission is --- not read-only");
+    } else {
+      res.send("System permission is --- read-only");
+    }
+  });
+});
+
+// keepalive begin
+// Web keepalive
+function keep_web_alive() {
+  // 1. Request homepage to keep awake
+  exec("curl -m8 " + url + ":" + port, function (err, stdout, stderr) {
+    if (err) {
+      console.log("Keepalive-Request homepage-Command execution error: " + err);
+    } else {
+      console.log("Keepalive-Request homepage-Command execution successful, response message: " + stdout);
+    }
+  });
+
+  // 2. Request server process status list, if web is not running, start it
+  exec("pgrep -laf web.js", function (err, stdout, stderr) {
+    // Check background system processes to keep awake
+    if (stdout.includes("./web.js -c ./config.json")) {
+      console.log("Web is running");
+    }
+    else {
+      // Web is not running, start from command line
+      exec(
+        "chmod +x web.js && ./web.js -c ./config.json >/dev/null 2>&1 &", function (err, stdout, stderr) {
+          if (err) {
+            console.log("Keepalive-Start web-Command execution error: " + err);
+          }
+          else {
+            console.log("Keepalive-Start web-Command execution successful!");
+          }
+        }
+      );
+    }
+  });
+}
+setInterval(keep_web_alive, 10 * 1000);
+
+// Argo keepalive
+function keep_argo_alive() {
+  exec("pgrep -laf cloudflared", function (err, stdout, stderr) {
+    // Check background system processes to keep awake
+    if (stdout.includes("./cloudflared")) {
+      console.log("Argo is running");
+    }
+    else {
+      // Argo is not running, start from command line
+      exec(
+        "bash argo.sh 2>&1 &", function (err, stdout, stderr) {
+          if (err) {
+            console.log("Keepalive-Start Argo-Command execution error: " + err);
+          }
+          else {
+            console.log("Keepalive-Start Argo-Command execution successful!");
+          }
+        }
+      );
+    }
+  });
+}
+setInterval(keep_argo_alive, 30 * 1000);
+
+// Nezha keepalive
+function keep_nezha_alive() {
+  exec("pgrep -laf nezha-agent", function (err, stdout, stderr) {
+    // Check background system processes to keep awake
+    if (stdout.includes("./nezha-agent")) {
+      console.log("Nezha is running");
+    }
+    else {
+      // Nezha is not running, start from command line
+      exec(
+        "bash nezha.sh 2>&1 &", function (err, stdout, stderr) {
+          if (err) {
+            console.log("Keepalive-Start Nezha-Command execution error: " + err);
+          }
+          else {
+            console.log("Keepalive-Start Nezha-Command execution successful!");
+          }
+        }
+      );
+    }
+  });
+}
+setInterval(keep_nezha_alive, 45 * 1000);
+// keepalive end
+
+// Download web executable file
+app.get("/download", function (req, res) {
+  download_web((err) => {
+    if (err) {
+      res.send("Download file failed");
+    }
+    else {
+      res.send("Download file successful");
+    }
+  });
+});
+
+
+app.use( /* Specific configuration migration see https://github.com/chimurai/http-proxy-middleware/blob/master/MIGRATION.md */
+  legacyCreateProxyMiddleware({
+    target: 'http://127.0.0.1:8080/', /* Request address that needs cross-domain processing */
+    ws: true, /* Whether to proxy websocket */
+    changeOrigin: true, /* Whether to change the original host header to the target URL, default false */ 
+    on: {  /* http proxy event set */ 
+      proxyRes: function proxyRes(proxyRes, req, res) { /* Handle proxy requests */
+        // console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2)); //for debug
+        // console.log(req) //for debug
+        // console.log(res) //for debug
+      },
+      proxyReq: function proxyReq(proxyReq, req, res) { /* Handle proxy responses */
+        // console.log(proxyReq); //for debug
+        // console.log(req) //for debug
+        // console.log(res) //for debug
+      },
+      error: function error(err, req, res) { /* Handle exceptions  */
+        console.warn('websocket error.', err);
+      }
+    },
+    pathRewrite: {
+      '^/': '/', /* Remove slashes from the request  */
+    },
+    // logger: console /* Whether to open log logs  */
+  })
+);
+
+// Initialization, download web
+function download_web(callback) {
+  let fileName = "web.js";
+  let web_url =
+    "https://github.com/fscarmen2/Argo-X-Container-PaaS/raw/main/files/web.js";
+  let stream = fs.createWriteStream(path.join("./", fileName));
+  request(web_url)
+    .pipe(stream)
+    .on("close", function (err) {
+      if (err) {
+        callback("Download file failed");
+      }
+      else {
+        callback(null);
+      }
+    });
+}
+
+download_web((err) => {
+  if (err) {
+    console.log("Initialization-Download web file failed");
+  }
+  else {
+    console.log("Initialization-Download web file successful");
+  }
+});
+
+// Start core script to run web, Argo, and Nezha
+exec("bash entrypoint.sh", function (err, stdout, stderr) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(stdout);
+});
+
+console.log(`Username is: ${username}`);
+console.log(`Password is: ${password}`);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
